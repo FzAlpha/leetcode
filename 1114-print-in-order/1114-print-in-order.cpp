b953c21d2355745f9928fe395b8229d6f466ed01
FzAlpha <hritabratabardhan13579@gmail.com>
2026-06-08 19:16:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (42.96%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,36 @@
+class Foo {
+private:
+    int turn;
+    condition_variable cv;
+    mutex mtx;
+public:
+    Foo() {
+        turn = 1;
+    }
+
+    void first(function<void()> printFirst) {
+        unique_lock<mutex> lock(mtx);
+        // cv.wait(lock ,[this] {return turn == 1;});
+        // printFirst() outputs "first". Do not change or remove this line.
+        
+        printFirst();
+        turn = 2;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock ,[this] {return turn == 2;});
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        turn = 3;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock ,[this] {return turn == 3;});
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
